9ffa748428f4414f00ea44597b43e053bf34032c
TolgaFikri <tolgatur.f@gso.schule.koeln>
2023-01-12 08:53:27 +0100

    Klasse hinzugefuegt

---
+++ Verlag/Buch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verlag
+{
+    public class Buch
+    {
+        private string autor;
+        private string titel;
+        private int auflage;
+
+        public Buch(string autor,string titel) 
+        {
+            this.autor = autor;
+            this.titel = titel;
+            this.auflage = 1;
+
+        }
+
+        public Buch()
+        {
+
+        }
+
+
+
+
+    }
+}
